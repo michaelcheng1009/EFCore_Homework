c20a18185ef88ac62a280352659d8e426198b318
michaelcheng <a950a950@hotmail.com>
2019-12-13 19:35:04 +0800

    加入專案檔案。

---
+++ EFCore_Homework/Migrations/20191210103606_Person_Memo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore_Homework.Migrations
+{
+    public partial class Person_Memo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Memo",
+                table: "Person",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Memo",
+                table: "Person");
+        }
+    }
+}
